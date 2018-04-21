9fc2f21a7de161a609310144c656070af9f15dac
Euclides Fernandes <ef.webdev@gmail.com>
2018-04-21 05:16:56 -0300

    Add a few user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,5 +18,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a name, username, email and avatar_url' do
+    user = FactoryBot.build(:user)
+
+    expect(user).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    user = FactoryBot.build(:user, name: nil)
+    user.valid?
+
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is invalid without a username' do
+    user = FactoryBot.build(:user, username: nil)
+    user.valid?
+
+    expect(user.errors[:username]).to include("can't be blank")
+  end
+
+  it 'is invalid without an email' do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it 'is invalid without an avatar_url' do
+    user = FactoryBot.build(:user, avatar_url: nil)
+    user.valid?
+
+    expect(user.errors[:avatar_url]).to include("can't be blank")
+  end
 end
